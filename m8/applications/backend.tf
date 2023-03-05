265ce83ac42aea7e49656b330c52cd2d39f1a485
Vladislav Petrov <vpetrov@intermedia.com>
2023-03-05 22:57:28 +0400

    replace all host.docker.internal references

--- m8/applications/backend.tf
+++ m8/applications/backend.tf
@@ -3,7 +3,7 @@
 ##################################################################################
 terraform {
   backend "consul" {
-    address = "host.docker.internal:8500"
+    address = "127.0.0.1:8500"
     scheme  = "http"
   }
 }
